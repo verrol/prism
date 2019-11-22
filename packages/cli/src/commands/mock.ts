5c0a1488edbe8868a46c184f7d844e4da85858c7
Karol Maciaszek <karol@maciaszek.pl>
2019-11-22 13:20:10 +0100

    fix: unneeded array

--- packages/cli/src/commands/mock.ts
+++ packages/cli/src/commands/mock.ts
@@ -21,13 +21,13 @@ const mockCommand: CommandModule = {
           boolean: true,
           default: false,
         },
-        ['callback-delay']: {
+        'callback-delay': {
           description: 'Number of seconds to wait before executing callbacks',
           default: '0',
           string: true,
           coerce: (input: string) => toMillis(coerceRange(input)),
         },
-        ['callback-count']: {
+        'callback-count': {
           description: 'How many times a callback should be executed',
           default: '1',
           string: true,
